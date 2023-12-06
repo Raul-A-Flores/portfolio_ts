32b9fbf2129132a7c76024758aa4d97463b32036
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-12-06 15:02:58 -0800

    work

--- container/Work/Work.tsx
+++ container/Work/Work.tsx
@@ -98,7 +98,7 @@ const Work = () => {
               </p>
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>
-                  {work.tags[0]}</p>
+                  {work?.tags[0]}</p>
               </div>
             </div>
 
